76223b85f1be830ff277be1c9aa9383260a74cde
SnowCutieOwO <2210609731@qq.com>
2024-05-06 19:03:16 +0800

    Added even more function 现在它的字数统计功能几乎与 eteste.com 的一样了！

--- hiCount.py
+++ hiCount.py
@@ -10,15 +10,19 @@ def is_chinese(char):
         return False
 
 def hiCount(s,debug=False):
-    count,lCount = 0,0
+    count,lCount,elCount,nlCount = 0,0,0,0
     ignore_ = ["，","、","“","”","（","）","？","！","。","—","：","《","》","【","】","；"]
     for i in s:
         if is_chinese(i) and i not in ignore_:
             count += 1
         elif i in ignore_:
             lCount += 1
-    print("字数",count,"中文字符数",lCount)
-    return (count,lCount)
+        elif 'a' < i < 'z' or 'A' < i < 'Z':
+            elCount += 1
+        elif '0' < i < '9':
+            nlCount += 1
+    print("字数",count,"中文字符数",lCount,"英文字母数",elCount,"数字字符数",nlCount)
+    return (count,lCount,elCount,nlCount)
 
 def openRead(name,debug=False):
     # print(os.getcwd())
@@ -40,17 +44,20 @@ def foundFiles(name,debug=False):
     return (False,None)
 
 def __main__():
-    count = [0,0]
+    count = [0,0,0,0]
     a = foundFiles(input('请输入要计数的维基名称: '))
     if a[0]:
         for wj in a[1]:
             # 遍历并打开文件
             countFiles = openRead(wj)
             print(wj,end=" ")
-            count[0] += hiCount(countFiles)[0]
-            count[1] += hiCount(countFiles)[1]
+            product = hiCount(countFiles)
+            count[0] += product[0]
+            count[1] += product[1]
+            count[2] += product[2]
+            count[3] += product[3]
         else:
-            print('共',count[0],'字,','其中汉字',count[0]-count[1],'字,','中文标点符号',count[1],'字')
+            print('共',count[0],'字,','其中汉字',count[0]-count[1],'字,','中文标点符号',count[1],'字,','英文字母',count[2],'字,','数字符号',count[3],'字')
 
 __main__()
         
