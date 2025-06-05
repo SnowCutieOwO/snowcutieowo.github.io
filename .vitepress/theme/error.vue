573ec8690b61ce2385be0a71e32e210379001aba
SnowCutieOwO <2210609731@qq.com>
2025-06-05 12:42:20 +0800

    chore (wiki): updated error page

--- .vitepress/theme/error.vue
+++ .vitepress/theme/error.vue
@@ -14,6 +14,7 @@ const getRandomNum = Math.floor(Math.random() * (10))
         <p v-if="getRandomNum <= 1"><img src="/src/poop_original.gif" style="padding: 20px;"></p>
         <div><font size=20; style='border: 20px;'>404</font></div>
         <p>你要找的页面不存在。:(</p>
+        <a href="/">返回首页</a>
     </center>  
 </template>
 
